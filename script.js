39a5a0d8ccb25bdfd3baadd1137868dec03835e9
Ibrahim Ismail <ebrahemesmael@gmail.com>
2024-09-15 07:14:32 +0300

    98.css styles and more

--- script.js
+++ script.js
@@ -10,18 +10,22 @@ let tenPixels = `
     <div class="pixel"></div>
     <div class="pixel"></div>
     <div class="pixel"></div>
-
 `;
 
-// for (let i = 0; i < 100; i++) {
-//     pixelGrid += tenPixels;
-// }
-document.querySelector('.container').innerHTML = pixelGrid;
 
+
+for (let i = 0; i < 100; i++) {
+    pixelGrid += tenPixels;
+}
+let updatedPixels = 0
+document.querySelector('.pixelGrid').innerHTML = pixelGrid;
+let selectedColorBox = document.querySelector('.selected');
 let selectedColor = 'rgb(0 ,0 ,0)'
 document.querySelectorAll('.color').forEach((color) => {
     color.addEventListener('click', () => {
         selectedColor = `${color.classList[1]}`;
+        selectedColorBox.classList.remove('rgb(0,0,0)');
+        selectedColorBox.style.backgroundColor = `${selectedColor}`;
     })
 })
 
@@ -29,6 +33,8 @@ let selectedPixel = '';
 document.querySelectorAll('.pixel').forEach((div) => {
     div.addEventListener('click', () => {
         div.style.backgroundColor = `${selectedColor}`;
+        updatedPixels++
+        document.querySelector('.updatedPixels').innerHTML = `<p>updatedPixels: ${updatedPixels}</p>`;
     })
 })
 document.querySelectorAll('button').forEach((button) => {
@@ -36,6 +42,22 @@ document.querySelectorAll('button').forEach((button) => {
     button.style.backgroundColor = `${buttonClolor}`;
 });
 
+const infoWindow = document.querySelector('.infoWindow');
+const nowWhatWindow = document.querySelector('.nowWhatWindow');
+
 document.querySelector('.Help').addEventListener('click', () => {
-    document.querySelector('.window').classList.remove('hidden');
+    infoWindow.classList.remove('hidden');
+})
+document.querySelector('.Close').addEventListener('click', () => {
+    infoWindow.classList.add('hidden');
+})
+document.querySelector('.Close2').addEventListener('click', () => {
+    nowWhatWindow.classList.add('hidden');
+})
+
+
+
+document.querySelector('.clear').addEventListener('click', () => {
+    nowWhatWindow.classList.remove('hidden');
+    document.querySelector('.page').innerHTML = ''
 })
